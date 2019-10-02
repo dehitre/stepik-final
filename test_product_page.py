d1ab58a0968b3c3062a90c498beddbb9332d477b
kathie <esmirnova@okko.tv>
2019-10-02 09:58:01 +0300

    fix test

--- test_product_page.py
+++ test_product_page.py
@@ -63,8 +63,8 @@ class TestGuestAddToBasketFromProductPage:
         login_page.should_be_login_page()
 
     @pytest.mark.need_review
-    def test_guest_cant_see_product_in_basket_opened_from_main_page(self, browser):
-        link = "http://selenium1py.pythonanywhere.com"
+    def test_guest_cant_see_product_in_basket_opened_from_product_page(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207"
         page = ProductPage(browser, link)
         page.open()
         page.go_to_basket_page()
